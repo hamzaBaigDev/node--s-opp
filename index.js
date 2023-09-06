ed40ae90ca58cd432d81f675706ec869070b81d5
unknown <you@example.com>
2023-09-07 00:52:00 +0500

    pratice git

---
+++ index.js
@@ -0,0 +1,25 @@
+// lib //
+
+const express = require('express');
+const app = express();
+
+
+const port = 9090
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
+
+
+app.get('/hasan', (req, res) => 
+res.send('wo ghr py nhi hain'));
+
+
+
+app.post('/hasanPOST', (req, res) =>{
+
+    let name = req.body.name;
+    res.send(name);
+
+})
+
